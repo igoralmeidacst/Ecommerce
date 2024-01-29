472cc71cdf500e588f93f7fa9cfa4b7c7415a3ee
Igor Almeida <almeidaigor617@hotmail.com>
2024-01-29 11:22:13 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,15 +9,17 @@ class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
-	public function __construct($opts = array()) {
+	public function __construct($opts = array(), $tpl_dir = "/views/") {
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"         => false
 		);
@@ -28,7 +30,7 @@ class Page {
 
 	$this->setData($this->options["data"]);
 
-	$this->tpl->draw("header");
+	if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -52,7 +54,7 @@ class Page {
 
 	public function __destruct() {
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 
