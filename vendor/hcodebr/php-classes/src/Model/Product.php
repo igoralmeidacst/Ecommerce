8fbef7ca8a294bec6aaca1ae94c7b261141e9bd3
Igor Almeida <almeidaigor617@hotmail.com>
2024-02-20 13:26:15 -0300

    Esqueci a senha completo, mas não chega e-mail

---
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -0,0 +1,66 @@
+<?php  
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+use \Hcode\Mailer;
+
+class Product extends Model {
+
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
+
+	}
+
+	public function save()
+	{
+
+		$sql = new Sql();
+ 		
+ 		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", array(
+ 			":idproduct"=>$this->getidproduct(),
+ 			":desproduct"=>$this->getdesproduct(),
+ 			":vlprice"=>$this->getvlprice(),
+ 			":vlwidth"=>$this->getvlwidth(),
+ 			":vlheight"=>$this->getvlheight(),
+ 			":vllenght"=>$this->getvllenght(),
+ 			":vlweight"=>$this->getvlweight(),
+ 			":desurl"=>$this->getdesurl()
+ 		));
+
+ 		$this->setData($results[0]);
+
+	}
+
+	public function get($idproduct)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct", [
+			':idproduct'=>$idproduct
+		]);
+
+		$this->setData($results[0]);
+
+	}
+
+	public function delete()
+	{
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct", [
+			':idproduct'=>$this->getidproduct()
+		]);
+
+	}
+
+}
+
+?>
