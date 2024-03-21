0cabf169a17d82bcaa52939e1126dbc4ef957320
Igor Almeida <almeidaigor617@hotmail.com>
2024-03-21 00:30:58 -0300

    Integração do paypal OK, ARRUMAR pagseguro

--- site.php
+++ site.php
@@ -193,7 +193,7 @@ $app->get("/checkout", function(){
 
 });
 
-$app->post("/checkout", function(){
+$app->post("/checkout", function () {
 
 	User::verifyLogin(false);
 
@@ -260,9 +260,66 @@ $app->post("/checkout", function(){
 
 	$order->save();
 
-	header("Location: /order/".$order->getidorder());
-	exit;
+	switch ((int)$_POST['payment-method']) {
 
+		case 1:
+			header("Location: /order/" . $order->getidorder() . "/pagseguro");
+			break;
+
+		case 2:
+			header("Location: /order/" . $order->getidorder() . "/paypal");
+			break;
+	}
+
+	exit;
+});
+
+$app->get("/order/:idorder/pagseguro", function ($idorder) {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header' => false,
+		'footer' => false
+	]);
+
+	$page->setTpl("payment-pagseguro", [
+		'order' => $order->getValues(),
+		'cart' => $cart->getValues(),
+		'products' => $cart->getProducts(),
+		'phone' => [
+			'areaCode' => substr($order->getnrphone(), 0, 2),
+			'number' => substr($order->getnrphone(), 2, strlen($order->getnrphone()))
+		]
+	]);
+});
+
+$app->get("/order/:idorder/paypal", function ($idorder) {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header' => false,
+		'footer' => false
+	]);
+
+	$page->setTpl("payment-paypal", [
+		'order' => $order->getValues(),
+		'cart' => $cart->getValues(),
+		'products' => $cart->getProducts()
+	]);
 });
 
 $app->get("/login", function(){
